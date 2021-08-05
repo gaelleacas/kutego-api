49e236f358a1b500a5b6a73102034305c33f20a4
gaelle.acas <gaelle.acas@gmail.com>
2021-08-05 20:05:16 +0200

    fix(assets): fix workdir on docker file

--- internal/main.go
+++ internal/main.go
@@ -153,7 +153,9 @@ func GetGopherRandom(gopher operations.GetGopherRandomParams) middleware.Respond
 Display Fire Gopher with a message (error)
 */
 func getFireGopherError(message string) (image.Image, error) {
-	file, err := os.Open("assets/fire-gopher.png")
+
+	// Open local file
+	file, err := os.Open("./assets/fire-gopher.png")
 	if err != nil {
 		log.Fatalf("failed to Open fire-gopher image: %v", err)
 	}
@@ -163,7 +165,10 @@ func getFireGopherError(message string) (image.Image, error) {
 		log.Fatalf("failed to Decode image: %v", err)
 		return srcImage, err
 	}
+	// Add Text on Gopher
 	srcImage, err = TextOnGopher(srcImage, "Ooops, Error! It's on fire!")
+
+	// Resize Image
 	srcImage = resizeImage(srcImage, "medium")
 	if err != nil {
 		log.Fatalf("failed to put Text on Gopher: %v", err)
